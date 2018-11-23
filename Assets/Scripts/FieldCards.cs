f43a8dffb8f9c5234db8518e5ead888004642b24
Gerson Bello <gersonbellodegoes@gmail.com>
2018-11-23 16:24:35 -0200

    Base Cards 0.51

---
+++ Assets/Scripts/FieldCards.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldCards : MonoBehaviour {
+
+    public GameObject Card;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		
+	}
+
+    void SetFieldCard()
+    {
+        Card.SetActive(true);
+        Card.SendMessage("ReciveCardsSetings",CardsController.cardSelectedConfig);
+    }
+}
